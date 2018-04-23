d6451b9ca34152b6b20b83e4fbf004e8cfa6678d
Khai Nguyen <khai.nguyen@outlook.com>
2018-04-23 00:44:43 -0700

    Add message comp & service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -5,14 +5,16 @@ import { of } from 'rxjs/observable/of';
 
 import { Hero } from '../shared/models/hero';
 import { HEROES } from '../../assets/mock-heroes';
+import { MessageService } from '../services/message.service';
 
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private _messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this._messageService.add('HeroService: fetched the heroes');
     return of(HEROES);
   }
 }
