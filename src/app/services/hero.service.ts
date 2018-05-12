bbc36750ed052cd6f5fcef6bb090f73fcd6dcaab
Khai Nguyen <khai.nguyen@outlook.com>
2018-05-12 15:27:42 -0700

    Mock data provider service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 import { Hero } from '../shared/models/hero';
-import { HEROES } from '../../assets/mock-heroes';
 import { MessageService } from '../services/message.service';
 
 
 @Injectable()
 export class HeroService {
 
-  constructor(private _messageService: MessageService) { }
+  private _heroesUrl = 'api/heroes';
+
+  constructor(private _httpClient: HttpClient, private _messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    this._messageService.add(`HeroService: fetched ${HEROES.length} heroes`);
-    return of(HEROES);
+    // this._messageService.add(`HeroService: fetched ${HEROES.length} heroes`);
+    return this._httpClient.get<Hero[]>(this._heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this._messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string) {
+    this._messageService.add('HeroService: ' + message);
   }
 }
