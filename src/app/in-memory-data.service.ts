4b12d98be1a4479a39a42349530b3897cef819a1
Khai Nguyen <knguyen2@costar.com>
2019-08-29 10:48:22 -0700

    Fix build error

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,5 +1,7 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Hero } from './shared/models/hero';
+
 export class InMemoryDataService implements InMemoryDbService {
 
   createDb() {
@@ -15,6 +17,15 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return {heroes};
+    return { heroes };
+  }
+
+  /**
+   * Overrides the genId method to ensure that a hero always has an id.
+   * If the heroes array is empty, return the initial number (11).
+   * If the heroes array is not empty, return the highest hero id + 1.
+   */
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
+import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
@@ -17,12 +16,35 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     // this._messageService.add(`HeroService: fetched ${HEROES.length} heroes`);
-    return this._httpClient.get<Hero[]>(this._heroesUrl);
+    return this._httpClient
+      .get<Hero[]>(this._heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    this._messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this._heroesUrl}/${id}`;
+    return this._httpClient
+      .get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`fetched hero id ${id}`)),
+        catchError(this.handleError<Hero>(`getHero id ${id}`))
+      );
+  }
+  /**
+   * Handles HTTP operation that failed
+   * Lets the app continue
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 
   private log(message: string) {
